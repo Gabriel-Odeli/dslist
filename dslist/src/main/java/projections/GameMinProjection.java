9fc960f4d1adb8220760f4dfe2c3c3f48184b021
Gabiru-s <gabrielodeli8@gmail.com>
2024-11-12 19:53:45 -0300

    Consulta SQL, projection, busca de games por lista

---
+++ dslist/src/main/java/projections/GameMinProjection.java
@@ -0,0 +1,11 @@
+package projections;
+
+public interface GameMinProjection {
+
+    long getId();
+    String getTitle();
+    int getYear();
+    String getImgUrl();
+    String getShortDescription();
+    int getPosition();
+}
